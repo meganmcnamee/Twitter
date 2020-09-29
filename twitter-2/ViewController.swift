b7020a3405322345abdcf0ff761e8fdbc9044229
Megan McNamee <meganrosemcnaamee@gmail.com>
2020-09-29 13:43:17 -0700

    Initial Commit

---
+++ twitter-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-2
+//
+//  Created by user178618 on 9/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ twitter-2Tests/twitter_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  twitter_2Tests.swift
+//  twitter-2Tests
+//
+//  Created by user178618 on 9/29/20.
+//
+
+import XCTest
+@testable import twitter_2
+
+class twitter_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
